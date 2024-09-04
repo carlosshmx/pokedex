3ad267a718958d45903932c3c01e53b7b074d0ec
Carlos Colmenares <carlosshmx@hotmail.com>
2024-09-03 22:50:23 -0300

    mejorado de loading

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
-
+import pokedex from '../../assets/pokedex.png';
+import homeStyle from './home.module.css';
 
 const Home = () => {
   return (
     <div>
-      <h1>HOME</h1>
+      <div className={homeStyle.panel1}>
+        <img src={pokedex} alt="Pokedex" />
+        <div className={homeStyle.panelCard}>
+          <h2>Bienvenido</h2>
+          <p>Esta web fue desarrollada con react </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import styles from './loading.module.css'
 
 const Loading = () => {
   return (
-    <div>
+    <div className={styles.loadingContainer}>
         <h2>Cargando...</h2>
         <img className={styles.errorImg} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/512px-Pok%C3%A9_Ball_icon.svg.png?20161023215848" alt="" />
     </div>
