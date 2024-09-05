565347ef04895ac949157dfb74576c8442453d74
Carlos Colmenares <carlosshmx@hotmail.com>
2024-09-05 11:46:11 -0300

    agregados imagenes de tipos

--- src/Utils/Pokemon.js
+++ src/Utils/Pokemon.js
@@ -42,102 +42,109 @@ export const types = [
     {
         name: "normal",
         color: "#A8A77A", // Normal
-        url: "https://pokeapi.co/api/v2/type/1/"
+        url: "https://pokeapi.co/api/v2/type/1/",
+        img: "https://archives.bulbagarden.net/media/upload/e/e6/NormalIC_Masters.png"
     },
     {
         name: "fighting",
         color: "#C22E28", // Fighting
-        url: "https://pokeapi.co/api/v2/type/2/"
+        url: "https://pokeapi.co/api/v2/type/2/",
+        img: "https://archives.bulbagarden.net/media/upload/0/06/FightingIC_Masters.png"
     },
     {
         name: "flying",
         color: "#A98FF3", // Flying
-        url: "https://pokeapi.co/api/v2/type/3/"
+        url: "https://pokeapi.co/api/v2/type/3/",
+        img: "https://archives.bulbagarden.net/media/upload/2/2f/FlyingIC_Masters.png"
     },
     {
         name: "poison",
         color: "#A33EA1", // Poison
-        url: "https://pokeapi.co/api/v2/type/4/"
+        url: "https://pokeapi.co/api/v2/type/4/",
+        img: "https://archives.bulbagarden.net/media/upload/f/f3/PoisonIC_Masters.png"
     },
     {
         name: "ground",
         color: "#E2BF65", // Ground
-        "url": "https://pokeapi.co/api/v2/type/5/"
+        url: "https://pokeapi.co/api/v2/type/5/",
+        img: "https://archives.bulbagarden.net/media/upload/7/74/GroundIC_Masters.png"
     },
     {
         name: "rock",
         color: "#B6A136", // Rock
-        url: "https://pokeapi.co/api/v2/type/6/"
+        url: "https://pokeapi.co/api/v2/type/6/",
+        img: "https://archives.bulbagarden.net/media/upload/9/9e/RockIC_Masters.png"
     },
     {
         name: "bug",
         color: "#A6B91A", // Bug
         url: "https://pokeapi.co/api/v2/type/7/",
-        img: "https://archives.bulbagarden.net/media/upload/9/9c/Bug_icon_SwSh.png"
+        img: "https://archives.bulbagarden.net/media/upload/8/82/BugIC_Masters.png"
     },
     {
         name: "ghost",
         color: "#735797", // Ghost
-        url: "https://pokeapi.co/api/v2/type/8/"
+        url: "https://pokeapi.co/api/v2/type/8/",
+        img: "https://archives.bulbagarden.net/media/upload/6/68/GhostIC_Masters.png"
     },
     {
         name: "steel",
         color: "#B7B7CE", // Steel
-        url: "https://pokeapi.co/api/v2/type/9/"
+        url: "https://pokeapi.co/api/v2/type/9/",
+        img: "https://archives.bulbagarden.net/media/upload/3/39/SteelIC_Masters.png"
     },
     {
-        "name": "fire",
-        "color": "#EE8130", // Fire
-        "url": "https://pokeapi.co/api/v2/type/10/"
+        name: "fire",
+        color: "#EE8130", // Fire
+        url: "https://pokeapi.co/api/v2/type/10/",
+        img: "https://archives.bulbagarden.net/media/upload/2/2e/FireIC_Masters.png"
     },
     {
         name: "water",
         color: "#6390F0", // Water
-        url: "https://pokeapi.co/api/v2/type/11/"
+        url: "https://pokeapi.co/api/v2/type/11/",
+        img: "https://archives.bulbagarden.net/media/upload/3/3f/WaterIC_Masters.png"
     },
     {
         name: "grass",
         color: "#7AC74C", // Grass
-        url: "https://pokeapi.co/api/v2/type/12/"
+        url: "https://pokeapi.co/api/v2/type/12/",
+        img: "https://archives.bulbagarden.net/media/upload/3/32/GrassIC_Masters.png"
     },
     {
         name: "electric",
         color: "#F7D02C", // Electric
-        url: "https://pokeapi.co/api/v2/type/13/"
+        url: "https://pokeapi.co/api/v2/type/13/",
+        img: "https://archives.bulbagarden.net/media/upload/1/1a/ElectricIC_Masters.png"
     },
     {
         name: "psychic",
         color: "#F95587", // Psychic
-        url: "https://pokeapi.co/api/v2/type/14/"
+        url: "https://pokeapi.co/api/v2/type/14/",
+        img: "https://archives.bulbagarden.net/media/upload/9/99/PsychicIC_Masters.png"
     },
     {
         name: "ice",
         color: "#96D9D6", // Ice
-        url: "https://pokeapi.co/api/v2/type/15/"
+        url: "https://pokeapi.co/api/v2/type/15/",
+        img: "https://archives.bulbagarden.net/media/upload/9/9b/IceIC_Masters.png"
     },
     {
         name: "dragon",
         color: "#6F35FC", // Dragon
-        url: "https://pokeapi.co/api/v2/type/16/"
+        url: "https://pokeapi.co/api/v2/type/16/",
+        img: "https://archives.bulbagarden.net/media/upload/d/d7/DragonIC_Masters.png"
     },
     {
         name: "dark",
         color: "#705746", // Dark
-        url: "https://pokeapi.co/api/v2/type/17/"
+        url: "https://pokeapi.co/api/v2/type/17/",
+        img: "https://archives.bulbagarden.net/media/upload/4/43/DarkIC_Masters.png"
     },
     {
         name: "fairy",
         color: "#D685AD", // Fairy
-        url: "https://pokeapi.co/api/v2/type/18/"
+        url: "https://pokeapi.co/api/v2/type/18/",
+        img: "https://archives.bulbagarden.net/media/upload/f/fa/FairyIC_Masters.png"
     },
-    {
-        name: "stellar",
-        color: "#FFCE4B", // Hypothetical color for "stellar"
-        url: "https://pokeapi.co/api/v2/type/19/"
-    },
-    {
-        name: "unknown",
-        color: "#A8A8A8", // Unknown
-        url: "https://pokeapi.co/api/v2/type/10001/"
-    }
 ];
