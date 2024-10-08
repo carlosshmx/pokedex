99cc1c7a575f98f033c7f639129e47e9f79f98bb
Carlos Colmenares <carlosshmx@hotmail.com>
2024-10-07 23:54:52 -0300

    Arregladas rutas de imagenes

--- src/Utils/Pokemon.js
+++ src/Utils/Pokemon.js
@@ -43,108 +43,108 @@ export const types = [
         name: "normal",
         color: "#A8A77A", // Normal
         url: "https://pokeapi.co/api/v2/type/1/",
-        img: "src/assets/typeIcons/NormalIC_Masters.png"
+        img: "/assets/typeIcons/NormalIC_Masters.png"
     },
     {
         name: "fighting",
         color: "#C22E28", // Fighting
         url: "https://pokeapi.co/api/v2/type/2/",
-        img: "src/assets/typeIcons/FightingIC_Masters.png"
+        img: "/assets/typeIcons/FightingIC_Masters.png"
     },
     {
         name: "flying",
         color: "#A98FF3", // Flying
         url: "https://pokeapi.co/api/v2/type/3/",
-        img: "src/assets/typeIcons/FlyingIC_Masters.png"
+        img: "/assets/typeIcons/FlyingIC_Masters.png"
     },
     {
         name: "poison",
         color: "#A33EA1", // Poison
         url: "https://pokeapi.co/api/v2/type/4/",
-        img: "src/assets/typeIcons/PoisonIC_Masters.png"
+        img: "/assets/typeIcons/PoisonIC_Masters.png"
     },
     {
         name: "ground",
         color: "#E2BF65", // Ground
         url: "https://pokeapi.co/api/v2/type/5/",
-        img: "src/assets/typeIcons/GroundIC_Masters.png"
+        img: "/assets/typeIcons/GroundIC_Masters.png"
     },
     {
         name: "rock",
         color: "#B6A136", // Rock
         url: "https://pokeapi.co/api/v2/type/6/",
-        img: "src/assets/typeIcons/RockIC_Masters.png"
+        img: "/assets/typeIcons/RockIC_Masters.png"
     },
     {
         name: "bug",
         color: "#A6B91A", // Bug
         url: "https://pokeapi.co/api/v2/type/7/",
-        img: "src/assets/typeIcons/BugIC_Masters.png"
+        img: "/assets/typeIcons/BugIC_Masters.png"
     },
     {
         name: "ghost",
         color: "#735797", // Ghost
         url: "https://pokeapi.co/api/v2/type/8/",
-        img: "src/assets/typeIcons/GhostIC_Masters.png"
+        img: "/assets/typeIcons/GhostIC_Masters.png"
     },
     {
         name: "steel",
         color: "#B7B7CE", // Steel
         url: "https://pokeapi.co/api/v2/type/9/",
-        img: "src/assets/typeIcons/SteelIC_Masters.png"
+        img: "/assets/typeIcons/SteelIC_Masters.png"
     },
     {
         name: "fire",
         color: "#EE8130", // Fire
         url: "https://pokeapi.co/api/v2/type/10/",
-        img: "src/assets/typeIcons/FireIC_Masters.png"
+        img: "/assets/typeIcons/FireIC_Masters.png"
     },
     {
         name: "water",
         color: "#6390F0", // Water
         url: "https://pokeapi.co/api/v2/type/11/",
-        img: "src/assets/typeIcons/WaterIC_Masters.png"
+        img: "/assets/typeIcons/WaterIC_Masters.png"
     },
     {
         name: "grass",
         color: "#7AC74C", // Grass
         url: "https://pokeapi.co/api/v2/type/12/",
-        img: "src/assets/typeIcons/GrassIC_Masters.png"
+        img: "/assets/typeIcons/GrassIC_Masters.png"
     },
     {
         name: "electric",
         color: "#F7D02C", // Electric
         url: "https://pokeapi.co/api/v2/type/13/",
-        img: "src/assets/typeIcons/ElectricIC_Masters.png"
+        img: "/assets/typeIcons/ElectricIC_Masters.png"
     },
     {
         name: "psychic",
         color: "#F95587", // Psychic
         url: "https://pokeapi.co/api/v2/type/14/",
-        img: "src/assets/typeIcons/PsychicIC_Masters.png"
+        img: "/assets/typeIcons/PsychicIC_Masters.png"
     },
     {
         name: "ice",
         color: "#96D9D6", // Ice
         url: "https://pokeapi.co/api/v2/type/15/",
-        img: "src/assets/typeIcons/IceIC_Masters.png"
+        img: "/assets/typeIcons/IceIC_Masters.png"
     },
     {
         name: "dragon",
         color: "#6F35FC", // Dragon
         url: "https://pokeapi.co/api/v2/type/16/",
-        img: "src/assets/typeIcons/DragonIC_Masters.png"
+        img: "/assets/typeIcons/DragonIC_Masters.png"
     },
     {
         name: "dark",
         color: "#705746", // Dark
         url: "https://pokeapi.co/api/v2/type/17/",
-        img: "src/assets/typeIcons/DarkIC_Masters.png"
+        img: "/assets/typeIcons/DarkIC_Masters.png"
     },
     {
         name: "fairy",
         color: "#D685AD", // Fairy
         url: "https://pokeapi.co/api/v2/type/18/",
-        img: "src/assets/typeIcons/FairyIC_Masters.png"
+        img: "/assets/typeIcons/FairyIC_Masters.png"
     },
 ];
